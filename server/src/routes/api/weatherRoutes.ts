63171638d5a61806a6168fad474d5ac77113dea0
gamalmubarak <eng-gemy@hotmail.com>
2025-02-26 21:36:54 -0500

    GET search history

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -26,7 +26,16 @@ router.post('/', async (req: Request, res: Response) => {
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.getCities();
+
+    return res.status(200).json(history);
+  } catch (error) {
+    console.error('Error fetching search history:', error);
+    return res.status(500).json({ error: 'Unable to fetch search history' });
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {});
